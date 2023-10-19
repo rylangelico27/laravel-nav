6e905cd82f62b5d93b2aa3e19bda0c3fe2ed7760
rylangelico27 <rylanangelico.tioco.cics@ust.edu.ph>
2023-10-19 21:34:29 +0800

    first comit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+
+
+    <header class="header">
+        <a href="#" class="logo">Logo</a>
+
+        <nav class="navbar">
+            <a href="{{ url('/') }}">Home</a>
+            <a href="{{ url('about') }}">About Us</a>
+            <a href="{{ url('contact') }}">Contact</a>
+            <a href="{{ url('list') }}">List</a>
+            <a href="{{ url('services') }}">Services</a>
+        </nav>
+        <div class="content">
+            <h1>Welcome to Our Website</h1>
+            <p>This is the home page.</p>
+        </div>
+
+
+</body>
+
+</html>
